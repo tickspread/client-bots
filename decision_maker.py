1d113e9beb3f9c52bcdf4d6fbb0b64366cff53f5
Dmitry Podborits <dpodbori@gmail.com>
2021-11-06 07:35:52 -0700

    streamlined controller / decision maker interactions in accordance with the latest conversation.

--- decision_maker.py
+++ decision_maker.py
@@ -1,23 +1,13 @@
 from typing import List
 
-
 class DecisionMakerBase:
-    def __init__(self, *args, **kwds):
-        ...
 
     def updateExchOrder(self, exchOrder):
         ...
 
+    def updateFailedOrder(self, orderId):
+        ...
+
     def getTradingOrders(self) -> List:
         ...
-
-    def updateFailedOrder(self, orderId: int):
-        ...
-
-    def getCancelOrders(self) -> List:
-        ...
-
-
-
-
 
